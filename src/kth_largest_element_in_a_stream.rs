c92513fecf47637f3feac2431d483e19682e6dc2
btooom1999 <btooom1999@gmail.com>
2026-03-25 12:49:36 +0700

    finished kth_largest_element_in_a_stream exercise

---
+++ src/kth_largest_element_in_a_stream.rs
@@ -0,0 +1,39 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
+struct KthLargest {
+    k: i32,
+    min_heap: BinaryHeap<Reverse<i32>>,
+}
+
+impl KthLargest {
+    fn new(k: i32, nums: Vec<i32>) -> Self {
+        let mut min_heap = BinaryHeap::new();
+        for num in &nums {
+            min_heap.push(Reverse(*num));
+            if min_heap.len() as i32 > k {
+                min_heap.pop();
+            }
+        }
+
+        Self { k, min_heap }
+    }
+
+    fn add(&mut self, val: i32) -> i32 {
+        self.min_heap.push(Reverse(val));
+        if (self.min_heap.len() as i32) > self.k {
+            self.min_heap.pop();
+        }
+
+        let Reverse(kth) = self.min_heap.peek().unwrap();
+        *kth
+    }
+}
+
+pub fn main() {
+    let mut kthLargest = KthLargest::new(4, vec![7, 7, 7, 7, 8, 3]);
+    println!("{}", kthLargest.add(2)); // return 7
+    println!("{}", kthLargest.add(10)); // return 7
+    println!("{}", kthLargest.add(9)); // return 7
+    println!("{}", kthLargest.add(9)); // return 8
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#![allow(unused)]
+
+mod kth_largest_element_in_a_stream;
+
 fn main() {
-    println!("Hello, world!");
+    kth_largest_element_in_a_stream::main();
 }
